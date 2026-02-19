61ce4ded944b647132aa6ebcf611547ab45bcfb0
Shavir <shavirvallabh05@gmail.com>
2026-02-19 12:32:33 +0200

    first commit

---
+++ prac1/Vector.cpp
@@ -0,0 +1,65 @@
+#include "Vector.h"
+
+Vector::Vector(){
+    
+}
+
+Vector::Vector(std::initializer_list<float>){
+
+}
+
+Vector::Vector(float *){
+
+}
+
+Vector::~Vector(){
+
+}
+
+Vector::Vector(const Vector<n> &){
+
+}
+
+Vector::Vector(const Matrix<n,1>& ){
+
+}
+
+Vector<n>& Vector::operator=(const Vector<n>&){
+
+}
+
+Vector<n> Vector::operator+(const Vector<n>) const{
+
+}
+
+Vector<n> Vector::operator-(const Vector<n>) const{
+
+}
+
+Vector<n> Vector::operator*(const float) const{
+
+}
+
+float Vector::operator*(const Vector<n>) const{
+
+}
+
+float Vector::magnitude() const{
+
+}
+
+operator Vector::Matrix<n,1>() const{
+
+}
+
+Vector<3> Vector::crossProduct(const Vector<3>) const{
+
+}
+
+Vector<n> Vector::unitVector() const{
+
+}
+
+int Vector::getN() const{
+
+}
